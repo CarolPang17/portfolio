e9e16312fe4032f888354599d422deb3effec3c6
carolpang <carolpang17@gmail.com>
2022-05-06 10:49:35 -0700

    updated About me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,17 +3,10 @@ import coverImage from '../../assets/cover/cover-image.jpg';
 function About() {
   return (
     <section className="my-5">
-      <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <h1 id="about">About Me</h1>
       <div className="my-2">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
-          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
-          mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum.
-          Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
-          justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
-          dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh.
-          Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+            Welcome to Carol's personal page! I am a UCLA extention student with coding Bootcamp program! As a full stack web developer, i am looking to build a more intuitive user experience on the web. With the skills in JavaScript, CSS, React.js, I have builded several app that you are welcome to visit on the portfolio section!
         </p>
       </div>
     </section>
